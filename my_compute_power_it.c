9ca8767a8b24e0f9c1bcba49361e2ebc42287e07
Baron <arthur.baron@epitech.eu>
2019-10-04 17:02:21 +0200

    Task 03 my_compute_power_it

---
+++ my_compute_power_it.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2019
+** my_compute_power_it
+** File description:
+** Task 03
+*/
+
+int my_compute_power_it(int nb, int p)
+{
+    int value = nb;
+    for (int i = 1; i < p; i++) {
+        value = value * nb;
+    }
+} 
