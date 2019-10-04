6c3c1ba43e8a90a661cbb762535475f26e171040
Baron <arthur.baron@epitech.eu>
2019-10-04 22:33:21 +0200

    Task 06 my_is_prime

--- my_is_prime.c
+++ my_is_prime.c
@@ -11,9 +11,9 @@ int my_is_prime(int nb)
         return (0);
     }
     for (int i=2; i < nb; i++) {
-        if (nb % i == 0) { 
+        if (nb % i == 0) {
             return (0);
-        }            
+        }
     }
     return (1);
 }
