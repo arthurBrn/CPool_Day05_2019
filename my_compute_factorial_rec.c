32e03d2dbd050a5af3dc831049e333dc35d379bb
Baron <arthur.baron@epitech.eu>
2019-10-04 16:09:10 +0200

    Task 02 my_compute_factorial_rec

---
+++ my_compute_factorial_rec.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2019
+** my_compute_factorial_rec
+** File description:
+** Task 02
+*/
+
+int my_compute_factorial_rec(int nb)
+{
+    if (nb >= 0 && nb < 13) {
+        return(nb * my_compute_factorial_rec(nb - 1)); 
+    } else {
+        return (0);
+    }
+}
