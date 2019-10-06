d0f80a5d8ff964ea8f7f966f9e77d22bd3f2a21a
Baron <arthur.baron@epitech.eu>
2019-10-06 16:32:25 +0200

    Task 01 corrected on number 0 that was returning 0 instead of one.

--- my_compute_factorial_it.c
+++ my_compute_factorial_it.c
@@ -9,7 +9,10 @@ int my_compute_factorial_it(int nb)
 {
     int value = nb;
 
-    if (nb >= 0 && nb < 13) {
+    if (nb == 0)
+        return (1);
+
+    if (nb >= 0 && nb < 12) {
         for (int i=1;i < nb; i++) {
             value = value * (nb -i);
         }
