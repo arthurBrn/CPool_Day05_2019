e2247bcd1ace22cd3715df8b7b88213ba93f4c89
Baron <arthur.baron@epitech.eu>
2019-10-07 23:06:39 +0200

    my_compute_power_it modified

--- my_compute_power_it.c
+++ my_compute_power_it.c
@@ -9,15 +9,15 @@ int my_compute_power_it(int nb, int p)
 {
     int value = nb;
 
-    if (p == 0) {
+    if (p == 0)
         return (1);
+    if (p < 0 || nb < 0) {
+        return (0);
     }
-    if (p < 0) {
-        value = 0;
-    } else {
-        for (int i = 0; i < p; i++) {
-            value = value * nb;
-        }
+
+    for (int i = 1; i < p; i++) {
+        value = value * nb;
+//        p--;
     }
     return (value);
 }
