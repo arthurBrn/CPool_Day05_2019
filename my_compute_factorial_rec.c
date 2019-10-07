d5ed09268c0d19803e61e306159186b3943015a2
Baron <arthur.baron@epitech.eu>
2019-10-07 13:48:28 +0200

    Task 02 corrected

--- my_compute_factorial_rec.c
+++ my_compute_factorial_rec.c
@@ -7,6 +7,9 @@
 
 int my_compute_factorial_rec(int nb)
 {
+    if (nb == 0) {
+        return (1);
+    }
     if (nb >= 0 && nb < 13) {
         return(nb * my_compute_factorial_rec(nb - 1));
     } else {
